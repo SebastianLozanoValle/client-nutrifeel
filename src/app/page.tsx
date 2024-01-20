2c9dd093586ccaf69301f557eb52997cb735fac6
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2024-01-19 20:02:59 -0500

    primeros avances

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { NavBar } from "@/components/NavBar";
+import { Hero } from "@/components/Hero"
 
 
 export default function Home() {
   return (
     <main className="">
       <NavBar />
-      
+      <Hero />
     </main>
   )
 }
